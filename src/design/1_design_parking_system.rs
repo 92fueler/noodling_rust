8b1c2dc2e07523af83b43e65e237a03d8b526c0f
Joyce Wu <jianjoycewu@gmail.com>
2025-12-10 15:46:57 -0800

    parking system

--- src/design/1_design_parking_system.rs
+++ src/design/1_design_parking_system.rs
@@ -3,22 +3,34 @@
  */
 use std::collections::HashMap;
 
+#[repr(i32)]
+#[derive(Debug, Clone, Copy, Hash, Eq, PartialEq)]
+enum CarType {
+    Small = 1,
+    Medium = 2,
+    Big = 3,
+}
+
+#[derive(Debug)]
 struct ParkingSystem {
-    parking_hashmap: HashMap<i32, i32>,
+    parking_hashmap: HashMap<CarType, i32>,
 }
 
 impl ParkingSystem {
     fn new(big: i32, medium: i32, small: i32) -> Self {
-        let mut parking_hashmap = HashMap::new();
-        parking_hashmap.insert(1, big);
-        parking_hashmap.insert(2, medium);
-        parking_hashmap.insert(3, small);
+        let mut hm = HashMap::new();
+        hm.insert(CarType::Big, big);
+        hm.insert(CarType::Medium, medium);
+        hm.insert(CarType::Small, small);
 
-        ParkingSystem { parking_hashmap }
+        ParkingSystem {
+            parking_hashmap: hm,
+        }
     }
 
-    fn add_car(&mut self, car_type: i32) -> bool {
-        // Changed to &mut self
+    fn add_car(&mut self, car_type: CarType) -> bool {
+        // car_type shouldn't use i32 any more if using custom defined enum type
+
         if let Some(availability) = self.parking_hashmap.get_mut(&car_type) {
             if *availability == 0 {
                 return false;
@@ -29,4 +41,17 @@ impl ParkingSystem {
             false
         }
     }
+}
+
+fn main() {
+    let mut parking = ParkingSystem::new(1, 2, 3);
+    parking.add_car(CarType::Small);
+    parking.add_car(CarType::Small);
+
+    parking.add_car(CarType::Big);
+    parking.add_car(CarType::Big);
+
+    parking.add_car(CarType::Medium);
+
+    println!("{:?}", parking);
 }
